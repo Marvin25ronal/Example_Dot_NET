e293b9f484e8fc39fe4f53678fc4e886d34276a5
Marvin <marvin1ronal@gmail.com>
2021-06-04 13:06:52 -0600

    Creacion de la aplicacion

---
+++ aplicacion_consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aplicacion_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
